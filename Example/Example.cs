76997d460851d9820d6df7044b6f452d0f2901ab
lightbug14 <59814719+lightbug14@users.noreply.github.com>
2020-11-19 16:47:00 -0300

    Update Example.cs

--- Example/Example.cs
+++ Example/Example.cs
@@ -2,29 +2,44 @@ using UnityEngine;
 
 public class Example : MonoBehaviour
 {
+    public enum Reference
+    {
+        World ,
+        Local , 
+        External
+    }
+
+    [SerializeField]
+    Reference referenceMode = Reference.World;
+
+    [Condition( "referenceMode" , ConditionAttribute.ConditionType.IsEqualTo , ConditionAttribute.VisibilityType.Hidden , (int)Reference.External )]
+    [SerializeField]
+    Transform externalReference = null;
+
     [SerializeField]
     bool filterByTag = false;
-
     
     [Condition( "filterByTag" , ConditionAttribute.ConditionType.IsTrue , ConditionAttribute.VisibilityType.Hidden )]    
     [SerializeField]
     string filterTag = "SomeTag";
 
     [SerializeField]
-    bool useLayerMask = false;
+    bool activeLayerMask = false;
 
-    [Condition( "useLayerMask" , ConditionAttribute.ConditionType.IsTrue , ConditionAttribute.VisibilityType.NotEditable )]
+    [Condition( "activeLayerMask" , ConditionAttribute.ConditionType.IsTrue , ConditionAttribute.VisibilityType.NotEditable )]
     [SerializeField]
     LayerMask someLayerMask = 0;
 
-    public int speed = 0;
+    [SerializeField]
+    float speed = 0;
 
     [Condition( "speed" , ConditionAttribute.ConditionType.IsGreaterThan , ConditionAttribute.VisibilityType.Hidden , 5f )]
     public bool showHighSpeedWarning = true;
+
+
     
 
 }
-
 
 
 
